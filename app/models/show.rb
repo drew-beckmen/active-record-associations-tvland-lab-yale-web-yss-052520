0c9ba195ae9681db6b817d00a65dfc3bbd7402d8
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-07 10:28:23 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network 
+
+    # Use the method you already wrote in Actor class!
+    def actors_list 
+        self.actors.collect {|actor| actor.full_name}
+    end 
 end
